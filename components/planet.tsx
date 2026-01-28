26d55bd9243c08bc0777951081b3434a48fb6714
Julia Miodyńska <julia@MacBook-Pro-Julia.local>
2026-01-28 11:55:51 +0100

    No rotate implementation

--- components/planet.tsx
+++ components/planet.tsx
@@ -1,8 +1,9 @@
 import { useEffect, useMemo } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Animated, { Easing, useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
+import { Easing, StyleSheet, Text, View } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withRepeat, withTiming } from 'react-native-reanimated';
 
 const AnimatedText = Animated.createAnimatedComponent(Text);
+
 
 const EARTH_ORBIT_SPEED = 4000;
 const EARTH_SIZE = 80;
@@ -18,15 +19,25 @@ interface PlanetProps {
 }
 
 export const Planet = ({ symbol, color, distance = 1, size = 1, orbitSpeed = 1 }: PlanetProps) => {
-    const rotation = useSharedValue(0);
+
+    const progress = useSharedValue(0);
+
+
+    // 0 deg: [1, 0, 0, 1, 0, 0]
+    // 90 deg: [0, 1, -1, 0, 0, 0]
+    // 180 deg: [-1, 0, 0, -1, 0, 0]
+    // 270 deg: [0, -1, 1, 0, 0, 0]
+    // 360 deg: [1, 0, 0, 1, 0, 0]
+
 
     const planetSize = EARTH_SIZE * size;
     const duration = EARTH_ORBIT_SPEED * orbitSpeed;
-    const translateX = EARTH_DISTANCE * distance;
+    const distanceFromSun = EARTH_DISTANCE * distance;
+
 
     useEffect(() => {
-        rotation.value = withRepeat(
-            withTiming(360, {
+        progress.value = withRepeat(
+            withTiming(1, {
                 duration,
                 easing: Easing.linear,
             }),
@@ -35,7 +46,7 @@ export const Planet = ({ symbol, color, distance = 1, size = 1, orbitSpeed = 1 }
         );
     }, []);
 
-    const customPlanetStyle = useMemo(() => {
+    const planetStyle = useMemo(() => {
         return {
             backgroundColor: color,
             height: planetSize,
@@ -44,22 +55,41 @@ export const Planet = ({ symbol, color, distance = 1, size = 1, orbitSpeed = 1 }
         };
     }, [color, planetSize]);
 
-
+    const orbitStyle = useMemo(() => {
+        return {
+            height: 2 * distanceFromSun,
+            width: 2 * distanceFromSun,
+            borderColor: color,
+        };
+    }, [color, distanceFromSun]);
 
     const animatedStyle = useAnimatedStyle(() => {
+        const angle = progress.value * 2 * Math.PI;
+
+        const cos = Math.cos(angle);
+        const sin = Math.sin(angle);
+
+        const x = cos * distanceFromSun;
+        const y = sin * distanceFromSun;
+
         return {
             transform: [
-                { rotate: `${rotation.value}deg` },
-                { translateX },
-                { rotate: `${rotation.value}deg` },
-
+                { translateX: x },
+                { translateY: y },
+                { matrix: [cos, sin, -sin, cos, 0, 0] },
             ],
         };
     });
 
+
+
+
+
     return <>
-        <View style={[styles.orbit, { height: 2 * translateX, width: 2 * translateX, borderColor: color, opacity: 0.5 }]}></View>
-        <AnimatedText style={[styles.planet, customPlanetStyle, animatedStyle]}>{symbol}</AnimatedText>
+        <View style={[styles.orbit, orbitStyle]}></View>
+        <AnimatedText style={[styles.planet, planetStyle, animatedStyle]}>
+            {symbol}
+        </AnimatedText>
     </>
 };
 
@@ -80,6 +110,13 @@ const styles = StyleSheet.create({
         position: 'absolute',
         borderRadius: '50%',
         borderWidth: 1,
+        opacity: 0.5,
 
     },
+
+    planetContent: {
+        transformOrigin: 'center',
+        position: 'absolute',
+        backgroundColor: 'indianred',
+    }
 });
